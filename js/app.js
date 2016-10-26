8a0aa2b38be46fe8b057ce7ea7fec255f9008198
Andrew MacDonald <andrewmacdonald97@hotmail.com>
2016-10-26 12:12:57 -0300

    Merged the login and content together

--- js/app.js
+++ js/app.js
@@ -1,23 +1,25 @@
 
 var loggedUser = {};
+var database;
+var profileRef;
 
 $(document).ready(function() {
 
-// Initialize Firebase
-var config = {
-    apiKey: "AIzaSyC0L6HCDw3WNrnwxXkNRdwZ5ClvKZz8TKU",
-    authDomain: "pumpkinclicker.firebaseapp.com",
-    databaseURL: "https://pumpkinclicker.firebaseio.com",
-    storageBucket: "pumpkinclicker.appspot.com",
-    messagingSenderId: "102965890181"
-};
-firebase.initializeApp(config);
+    // Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyC0L6HCDw3WNrnwxXkNRdwZ5ClvKZz8TKU",
+        authDomain: "pumpkinclicker.firebaseapp.com",
+        databaseURL: "https://pumpkinclicker.firebaseio.com",
+        storageBucket: "pumpkinclicker.appspot.com",
+        messagingSenderId: "102965890181"
+    };
+    firebase.initializeApp(config);
 
-var auth = new firebase.auth();
-var facebookProvider = new firebase.auth.FacebookAuthProvider();
+    var auth = new firebase.auth();
+    var facebookProvider = new firebase.auth.FacebookAuthProvider();
 
-var database = new firebase.database();
-var profileRef = database.ref('/profiles');
+    database = new firebase.database();
+    profileRef = database.ref('/profiles');
 
 
 
@@ -52,7 +54,7 @@ var profileRef = database.ref('/profiles');
 						// way 1: objectname.objectvalue
 						// way 2: objectname['objectvalue']
 						if (snapshotValue[keys[i]].email == result.user.email) {
-							
+
 							// found the profile, access it
 							loggedUser = snapshotValue[keys[i]];
 							loggedUser.id = keys[i];
