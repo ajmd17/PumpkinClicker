57900282a96d1c73500db6f30b20e955d033df69
Andrew MacDonald <andrewmacdonald97@hotmail.com>
2016-10-26 11:02:08 -0300

    first commit

---
+++ js/app.js
@@ -0,0 +1,9 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyC0L6HCDw3WNrnwxXkNRdwZ5ClvKZz8TKU",
+    authDomain: "pumpkinclicker.firebaseapp.com",
+    databaseURL: "https://pumpkinclicker.firebaseio.com",
+    storageBucket: "pumpkinclicker.appspot.com",
+    messagingSenderId: "102965890181"
+};
+firebase.initializeApp(config);
